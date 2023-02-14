3755e6a0dcd57bb1b7cb5d89565b5fbcab44a800
harry hwang <tjdwls0475@hanmail.net>
2023-02-14 21:40:44 +0900

    Update 230214_buffer.go

--- 230214_buffer.go
+++ 230214_buffer.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-  // Here 'NewReader' function accepts 'string', and returns a 'pointer'
+        // Here 'NewReader' function accepts 'string', and returns a 'pointer'
 	r := strings.NewReader("I am Harry, and I am seeing Heyon.")
-  // 'make' is a built-in function in Golang that allows you to create slices, maps, and channels. The main purpose of it is to allocate a new, empty object and return a pointer.
+        // 'make' is a built-in function in Golang that allows you to create slices, maps, and channels. The main purpose of it is to allocate a new, empty object and return a pointer.
 	b := make([]byte, 4)
 
 	for {
